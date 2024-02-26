d2e2620cc06098d878ca4f6e705e90651af51c4e
liz <1543450579@qq.com>
2024-02-26 00:24:27 -0500

    add function list_space_node add "查询已有文档" logic

--- lark_client_functions.py
+++ lark_client_functions.py
@@ -1,6 +1,7 @@
 import lark_oapi as lark
 from lark_oapi.api.im import v1 as im_v1
 from lark_oapi.api.wiki import v2 as wiki_v2
+from lark_oapi.api.wiki.v2 import ListSpaceNodeRequest, ListSpaceNodeResponse
 
 
 def list_space_request(client: lark.Client):
@@ -21,13 +22,8 @@ def list_space_request(client: lark.Client):
     lark.logger.info(lark.JSON.marshal(response.data, indent=4))
 
     # todo 目前只返回了知识库名称列表，后续应该将知识库信息打包，完整返回
-    result = []  # 创建一个result的空列表
 
-    for item in response.data.items:  # 遍历 response.data.items
-        name_text = item.name
-        result.append(name_text)  # 将name_text的值添加到result
-
-    return result
+    return response.data.items  # 简化调用items的逻辑
 
 
 def send_text(text, user, client: lark.Client):
@@ -52,3 +48,25 @@ def send_text(text, user, client: lark.Client):
 
     # 处理业务结果
     lark.logger.info(lark.JSON.marshal(response.data, indent=4))
+
+
+def list_space_node(space_id, client: lark.Client):  # 定义新的函数，获取知识空间子节点列表
+
+    # 构造请求对象，根据技术文档需要传一个space_id
+    request: ListSpaceNodeRequest = ListSpaceNodeRequest.builder() \
+        .space_id(space_id) \
+        .build()
+
+    # 发起请求
+    response: ListSpaceNodeResponse = client.wiki.v2.space_node.list(request)
+
+    # 处理失败返回
+    if not response.success():
+        lark.logger.error(
+            f"client.wiki.v2.space_node.list failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
+        return
+
+    # 处理业务结果
+    lark.logger.info(lark.JSON.marshal(response.data, indent=4))
+
+    return response.data.items  # 对齐上面的items，直接调用
--- router.py
+++ router.py
@@ -1,9 +1,10 @@
 import lark_oapi.api.im.v1 as im_v1
 from flask import json
 from lark_oapi.api.application.v6 import P2ApplicationBotMenuV6
-
-from lark_client_functions import list_space_request, send_text
+from lark_client_functions import list_space_request, send_text, list_space_node
 from my_creator import LarkClientCreator
+
+
 def do_p2_im_message_receive_v1(data: im_v1.P2ImMessageReceiveV1) -> None:
     ctt = json.loads(data.event.message.content)  # 要把数据转成json
     text = ctt['text']  # 从json字典里面吧key为text的值取出来
@@ -21,7 +22,7 @@ def do_p2_im_message_receive_v1(data: im_v1.P2ImMessageReceiveV1) -> None:
             send_text(text=name, user=user, client=client)  # 调用text，user，client
 
 
-def do_boot_menu_event(data:P2ApplicationBotMenuV6) -> None:
+def do_boot_menu_event(data: P2ApplicationBotMenuV6) -> None:
     # print(lark.JSON.marshal(data))
     # 试着用一下飞书提供的接口吧少女！
     event_name = data.event.event_key
@@ -34,7 +35,24 @@ def do_boot_menu_event(data:P2ApplicationBotMenuV6) -> None:
 
     if event_name == '测试':
         send_text(event_name, user, client)
-    # elif event_name == "帮助"
+    elif event_name == "查询已有文档":
+        # 1. 获取知识库列表，将名称为“爱与幸福文档整理”的知识库数据捞出来，获取space_id
+        items = list_space_request(client)
+        for item in items:
+            if item.name == "爱与幸福文档整理":
+                space_id = item.space_id
+            else:
+                continue
+        # 2. 获取知识库内所有一级文档（查询知识库内文档）
+        nodes = list_space_node(space_id, client)
+        # 3. 将一级文档（标题+链接）以消息形式返回
+        text = ""  # 现在text为空
+        for node in nodes:  # 遍历node，传入title和url
+            title = node.title + ":"
+            url = "https://ti5z9w2m1c8.feishu.cn/wiki/" + node.node_token  # 飞书知识库文档的组成是前面的链接+node token
+            text += title + url + "\\n" # todo 为什么传"\\n"
+        send_text(text, user, client)  # 调用send text
+
 
     # todo 梳理文档知识库常用命令：查询已有文档 / 生成新模版文档
 
